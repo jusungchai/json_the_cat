5ef7c12df14618580501649ca528c5c68a1c754b
Jay Chai <chunsaa@gmail.com>
2019-10-22 17:44:30 +0000

    changed to function and exported for mocha chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,19 @@
 const request = require('request');
-const args = process.argv;
-const breed = args.slice(2)[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (!error) {
-    if (data.length === 0) {
-      console.log(`${breed} not found`);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (!error) {
+      if (data.length === 0) {
+        callback(null, `${breedName} not found`);
+      } else {
+        callback(null, data[0].description.trim());
+      }
     } else {
-      console.log(data[0].description);
+      callback(error);
     }
-  } else {
-    console.log(error);
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
